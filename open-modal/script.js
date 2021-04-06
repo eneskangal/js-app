ef7e675c2248e604d108e18041241ae3d7867913
enes <eneskangal@gmail.com>
2021-04-06 14:08:49 +0300

    dosyalar ekleme ve düzeltme

---
+++ open-modal/script.js
@@ -0,0 +1,8 @@
+function enes(){
+    document.getElementById("opent-modal").style.display="none"
+    document.getElementById("open-modal").style.display="block"
+}
+function enes2(){
+    document.getElementById("opent-modal").style.display="block"
+    document.getElementById("open-modal").style.display="none"
+}
--- sayıtekmiciftmi/yeni.js
+++ sayıtekmiciftmi/yeni.js
@@ -1,5 +1,8 @@
 var sayı=prompt("sayı giriniz");
-if(sayı%2==0){
+if(sayı==""){
+    alert("SAYI GİR LAN!")
+}
+else if(sayı%2==0){
     alert("sayı çift")
 }
 else{
