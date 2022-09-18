7380883c14035e241573c4e07c0e1417da8ff267
eneskangal <eneskangal@gmail.com>
2022-09-18 17:40:35 +0300

    bla bla bla
    blaaaaaaaaaaaaa

---
+++ searchRange/script.js
@@ -0,0 +1,19 @@
+let searchRange= function(nums, target){
+    let data=[]
+    let ilk=nums.indexOf(target)
+    let son=nums.lastIndexOf(target)
+    if(ilk<0){
+        data.push(-1)
+        data.push(-1)
+    }
+    else{
+        data.push(ilk)   
+        if(son<0){            
+            data.push(ilk)
+        }
+        else{            
+            data.push(son)
+        }
+    }
+    return data
+}
