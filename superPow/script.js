0bd1d362bb51f12086f1a59f0d079b986b3b61cc
eneskangal <eneskangal@gmail.com>
2023-05-20 12:56:42 +0300

    v1.4
    new programs loaded

---
+++ superPow/script.js
@@ -0,0 +1,12 @@
+let superPow= function(a, b){
+    let bInt = BigInt(b.join(""))
+    let m = 1337
+    let res = 1
+    a = a % m
+    while (bInt > 0){
+        if (bInt % 2n == 1) res = (res * a) % m
+        a = (a * a) % m
+        bInt = bInt >> 1n
+    }
+    return res
+}
